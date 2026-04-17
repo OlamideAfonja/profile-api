073d0c0c9dd16a4f03472b5e74ed28f367ebd5f9
OlamideAfonja <98019775+OlamideAfonja@users.noreply.github.com>
2026-04-17 20:03:35 +0100

    Update index.js

--- api/profiles/index.js
+++ api/profiles/index.js
@@ -1,5 +1,6 @@
 import { getAgeGroup, safeFetch } from "../../lib/utils.js";
 import { randomUUID } from "crypto";
+import { pool } from "../../lib/db.js";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
